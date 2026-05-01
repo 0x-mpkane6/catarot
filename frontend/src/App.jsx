911d537d0aaae16af6f5f034aee37a5c592f140b
phuckhang-infosec <phuckhang.infosec@gmail.com>
2026-05-01 16:25:15 +0700

    ADD: Signup page & Forgot password page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
+import SigninPage from "./pages/SigninPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <LoginPage />
+      <Routes>
+        {/* landing */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* auth */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SigninPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- frontend/src/pages/ForgotPasswordPage.jsx
+++ frontend/src/pages/ForgotPasswordPage.jsx
@@ -1,13 +1,13 @@
-import SigninForm from "../features/login/SigninForm.jsx";
+import ForgotPasswordForm from "../features/login/ForgotPasswordForm";
 import styles from "./LoginPage.module.css";
 
-export default function SigninPage() {
+export default function ForgotPasswordPage() {
   return (
     <div className={styles.page}>
       <div className={styles.overlay}></div>
 
       <div className={styles.wrapper}>
-        <SigninForm />
+        <ForgotPasswordForm />
       </div>
     </div>
   );
